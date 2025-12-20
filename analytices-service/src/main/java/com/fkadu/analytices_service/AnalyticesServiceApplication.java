fb4335c9f82caca83b11bc7b197344402570eaad
fk-du <fkadugoitomw@gmail.com>
2025-12-20 11:44:05 +0300

    analytics service

---
+++ analytices-service/src/main/java/com/fkadu/analytices_service/AnalyticesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fkadu.analytices_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyticesServiceApplication.class, args);
+	}
+
+}
---
+++ analytices-service/src/test/java/com/fkadu/analytices_service/AnalyticesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fkadu.analytices_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
